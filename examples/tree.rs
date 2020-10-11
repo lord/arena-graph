392061e930ea087831deb782e0ec97e41289df06
Robert Lord <lord@users.noreply.github.com>
2020-10-11 12:52:48 -0700

    format

--- examples/tree.rs
+++ examples/tree.rs
@@ -1,28 +1,26 @@
 use arena_graph::*;
-use std::sync::Arc;
 use std::cell::Cell;
-use std::ops::Deref;
 use std::cell::RefCell;
+use std::ops::Deref;
+use std::sync::Arc;
 
 struct TreeNode<T> {
     data: T,
     parent: Cell<NodePtr<Self>>,
-    children: RefCell<Vec<NodePtr<Self>>>
+    children: RefCell<Vec<NodePtr<Self>>>,
 }
 
 struct TreeNodeGuard<'gg, T>(NodeGuard<'gg, TreeNode<T>>);
 
-impl <'gg, T> Deref for TreeNodeGuard<'gg, T> {
+impl<'gg, T> Deref for TreeNodeGuard<'gg, T> {
     type Target = T;
     fn deref(&self) -> &T {
         &self.0.data
     }
 }
 
-impl <'gg, T> TreeNodeGuard<'gg, T> {
-    fn set_parent(&self, other: &Self) {
-    }
+impl<'gg, T> TreeNodeGuard<'gg, T> {
+    fn set_parent(&self, other: &Self) {}
 }
 
-fn main() {
-}
+fn main() {}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-use typed_arena::Arena;
+use std::cell::Cell;
 use std::marker::PhantomData;
 use std::ops::Deref;
-use std::cell::Cell;
+use typed_arena::Arena;
 
 pub struct Graph<N> {
     graph: Arena<N>,
@@ -17,24 +17,27 @@ pub struct NodeGuard<'gg, N> {
     invariant: PhantomData<&'gg mut &'gg ()>,
 }
 
-impl <'gg, N> Deref for NodeGuard<'gg, N> {
+impl<'gg, N> Deref for NodeGuard<'gg, N> {
     type Target = N;
     fn deref(&self) -> &N {
         self.inside
     }
 }
 
-impl <N> Graph<N> {
+impl<N> Graph<N> {
     pub fn new() -> Self {
         unimplemented!()
     }
 
     pub fn with<F: for<'any> FnOnce(GraphGuard<'any, N>)>(&self, func: F) {
-        func(GraphGuard {inside: self, invariant: PhantomData})
+        func(GraphGuard {
+            inside: self,
+            invariant: PhantomData,
+        })
     }
 }
 
-impl <'gg, N> GraphGuard<'gg, N> {
+impl<'gg, N> GraphGuard<'gg, N> {
     pub fn get(&self) -> NodeGuard<'gg, N> {
         unimplemented!()
     }
@@ -42,7 +45,7 @@ impl <'gg, N> GraphGuard<'gg, N> {
 
 pub struct NodePtr<N>(*const N);
 
-impl <'gg, N> NodeGuard<'gg, N> {
+impl<'gg, N> NodeGuard<'gg, N> {
     pub unsafe fn make_ptr(&self, other: &NodeGuard<'gg, N>) -> NodePtr<N> {
         NodePtr(other.inside as *const N)
     }
